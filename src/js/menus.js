ddfb370a3b564f109d44ab0f3b978bea38e97183
luis-mendes-95 <luis.mendes.95@hotmail.com>
2022-07-22 19:52:34 -0300

    efetuado alterações para mais dinamismo no cadastro de itens

---
+++ src/js/menus.js
@@ -0,0 +1,26 @@
+const headerMenu = [
+    {
+        nome: "Uniformes",
+        link: "./uniformes.html"
+    },
+    {
+        nome: "Camisetas personalizadas",
+        link: "./camisetas-personalizadas.html"
+    },
+    {
+        nome: "Camisetas lisas",
+        link: "./camisetas-lisas.html"
+    },
+    {
+        nome: "Windbanners",
+        link: "./windbanners.html"
+    },
+    {
+        nome: "Esportivo",
+        link: "./esportivo.html"
+    },
+    {
+        nome: "Moletons",
+        link: "./moletons.html"
+    }
+]
---
+++ src/js/products.js
@@ -0,0 +1,73 @@
+const products = [{
+    id: 1,
+    nome: "Camiseta personalizada estampa Brasil",
+    preco: "39,90",
+    categoria: "Camisetas personalizadas",
+    oferta: false,
+    estoque: true,
+    imgUrl: "./src/img/camiseta-personalizada-brasil-patriota.jpg"
+    },
+    {
+    id: 2,
+    nome: "Camiseta personalizada estampa Brasil Patriota",
+    preco: "39,90",
+    categoria: "Camisetas personalizadas",
+    oferta: false,
+    estoque: true,
+    imgUrl: "./src/img/camiseta-personalizada-brasil-patriota-2.jpg"  
+    },
+    {
+    id: 3,
+    nome: "Moletom personalizado com sua arte",
+    preco: "Solicitar orçamento",
+    categoria: ["Moletons","Personalizados"],
+    oferta: false,
+    estoque: true,
+    imgUrl: "./src/img/moletom-personalizado-com-sua-arte.jpg"  
+    },
+    {
+    id: 4,
+    nome: "Camiseta estampada feminina",
+    preco: "Solicitar orçamento",
+    categoria: "Personalizadas",
+    oferta: false,
+    estoque: true,
+    imgUrl: "./src/img/camiseta-estampada-feminina.jpg"  
+    },
+    {
+    id: 5,
+    nome: "Conjuntos de moletom",
+    preco: "Solicitar orçamento",
+    categoria: ["Moletons"],
+    oferta: false,
+    estoque: true,
+    imgUrl: "./src/img/conjuntos-de-moletom.jpg"  
+    },
+    {
+    id: 6,
+    nome: "Camiseta lisa verde",
+    preco: "Solicitar orçamento",
+    categoria: ["Camisetas Lisas"],
+    oferta: false,
+    estoque: true,
+    imgUrl: "./src/img/camiseta-lisa-verde.jpg"  
+    },
+    {
+    id: 7,
+    nome: "Camiseta lisa preta",
+    preco: "Solicitar orçamento",
+    categoria: ["Camisetas Lisas"],
+    oferta: false,
+    estoque: true,
+    imgUrl: "./src/img/camiseta-lisa-preta.jpg"  
+    },
+    {
+    id: 8,
+    nome: "Camiseta lisa verde manga comprida",
+    preco: "Solicitar orçamento",
+    categoria: ["Camisetas Lisas"],
+    oferta: false,
+    estoque: true,
+    imgUrl: "./src/img/camiseta-lisa-manga-comprida-verde.jpg"  
+    },
+]
